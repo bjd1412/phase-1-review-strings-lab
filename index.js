5960019ce52108706ea60cf4d28e48d4226d3d20
Brendan Dempsey <bjd1412@gmail.com>
2023-12-20 11:13:05 -0500

    String lab complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = "Brendan"
+const welcomeMessage = 'Welcome to Flatbook, Brendan!';
+
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, BRENDAN!';
+
+const shortGreeting = 'Welcome, B!'
